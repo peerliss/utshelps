e9d7674b8cf159c22a8a9800875530092ce8c119
peerliss <deependra1337@gmail.com>
2016-08-31 12:02:32 +1000

    Framework for project

--- app/src/main/java/com/utshelps/utshelpsapp/MyBookingActivity.java
+++ app/src/main/java/com/utshelps/utshelpsapp/MyBookingActivity.java
@@ -7,12 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class ProfileActivity extends AppCompatActivity {
+public class MyBookingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_my_booking);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
